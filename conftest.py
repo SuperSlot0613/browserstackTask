205cca2bed7e7bb38800bb836465c5e997ab2f58
Saurabh <saurabhyadav0613@gmail.com>
2025-09-16 21:42:44 +0530

    changes in code

--- conftest.py
+++ conftest.py
@@ -1,11 +1,18 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 #This Method is used for to send Driver Object to test method
 @pytest.fixture
 def driver():
-    driver=webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.maximize_window()
     driver.implicitly_wait(10)
     driver.get('https://www.amazon.in/')
