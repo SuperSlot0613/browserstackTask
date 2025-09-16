1c7457ef2deb4a635341b146c16a45952994ed93
Saurabh <saurabhyadav0613@gmail.com>
2025-09-16 21:52:11 +0530

    changes in code

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ RUN apt-get update && apt-get install -y \
     wget curl unzip git ca-certificates libcairo2 \
     libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libxkbcommon0 \
     libxcomposite1 libxrandr2 libxdamage1 libxfixes3 libxext6 \
-    libgbm1 libpango-1.0-0 libasound2 libx11-xcb1 libxcb1 \
+    libgbm1 libpango-1.0-0 libasound2 libx11-xcb1 libxcb1 chromium chromium-driver \
     && rm -rf /var/lib/apt/lists/*
 
 COPY requirements.txt .
--- conftest.py
+++ conftest.py
@@ -8,14 +8,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")  # or "--headless"
-    options.add_argument("--no-sandbox")  # important for Docker
-    options.add_argument("--disable-dev-shm-usage")  # prevents crashes
-    options.add_argument("--disable-gpu")  # safe for headless
-    options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(service=Service("/usr/bin/chromedriver"), options=options)
     driver.maximize_window()
     driver.implicitly_wait(10)
     driver.get('https://www.amazon.in/')
